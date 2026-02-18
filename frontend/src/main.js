4c066cf8dbd940f58b5f1a8a3498e1074ba53deb
Alex <georgealexandrusurugiu@gmail.com>
2026-02-18 22:32:29 +0200

    fast frontend setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,22 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' 
+import 'vuetify/styles'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
 
-app.mount('#app')
+app.mount('#app');
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+     {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../pages/HomePage.vue')
+    },
+    {
+      path: '/components',
+      name: 'components',
+      component: () => import('../pages/ComponentsPage.vue')
+    },
+    {
+      path: '/monitors',
+      name: 'monitors',
+      component: () => import('../pages/MonitorsPage.vue')
+    }
+  ],
 })
 
 export default router
