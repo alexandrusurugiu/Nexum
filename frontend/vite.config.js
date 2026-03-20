c983ae6d2a212691d01c9f9779023d2ffac1aa63
Alex <georgealexandrusurugiu@gmail.com>
2026-03-20 19:01:10 +0200

    frontend optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify({ autoImport: true }),
     vueDevTools(),
   ],
   resolve: {
